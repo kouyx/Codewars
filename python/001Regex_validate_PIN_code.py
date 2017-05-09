9de7949a485c61314826bc068bbbddca2e614291
Kou <bybynov09@gmail.com>
2017-05-10 00:04:27 +0800

    Change the clever and best practices into different version function.

--- python/001Regex_validate_PIN_code.py
+++ python/001Regex_validate_PIN_code.py
@@ -1,9 +1,13 @@
+import re
+
+
 def validate_pin(pin):
     """
     If 'pin' is a valid PIN string (containing exactly 4 or 6 digits), 
     return true, else return false.
     """
-    # return len(pin) in (4, 6) and pin.isdigit()
-    # return True only for exactly 4 or 6 digits
-    import re
     return bool(re.match(r'^(\d{4}|\d{6})$', pin))
+
+
+def validate_pin2(pin):
+    return len(pin) in (4, 6) and pin.isdigit()
--- python/002Find_the_divisors.py
+++ python/002Find_the_divisors.py
@@ -4,17 +4,20 @@ def divisors(integer):
     (except for 1 and the number itself). 
     If the number is prime return '(integer) is prime' 
     """
-    # Clever: return [i for i in xrange(2, integer) if not integer % i] or '%d is prime' % integer
     divisors = [x for x in xrange(2, integer) if integer % x == 0]
     if len(divisors) == 0:
         return '{} is prime'.format(integer)
     return divisors
 
-"""
-# Best practices
-def divisors(num):
-    l = [a for a in range(2,num) if num%a == 0]
+
+def divisors2(num):
+    # Clever
+    return [i for i in xrange(2, integer) if not integer % i] or '%d is prime' % integer
+
+
+def divisors3(num):
+    # Best practices
+    l = [a for a in range(2, num) if num % a == 0]
     if len(l) == 0:
         return str(num) + " is prime"
     return l
-"""
