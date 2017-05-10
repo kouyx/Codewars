3870ec50a61a93b1e7904b14fd5390fe75e06737
Kou <bybynov09@gmail.com>
2017-05-11 00:44:32 +0800

    1st attempt; Request Error in Codewars.

---
+++ python/Mix_Fruit_Juice.py
@@ -0,0 +1,30 @@
+import re
+
+
+def mix_fruit(arr):
+    """
+    Story
+    Jumbo Juice makes a fresh juice out of fruits of your choice. 
+    Jumbo Juice charges $5 for regular fruits and $7 for special ones. 
+    
+    Regular fruits are Banana, Orange, Apple, Lemon and Grapes. 
+    Special ones are Avocado, Strawberry and Mango. 
+    
+    Others fruits that are not listed are also available upon request. 
+    Those extra special fruits cost $9 per each. 
+    There is no limit on how many fruits she/he picks.
+    The price of a cup of juice is the mean of price of chosen fruits. 
+    In case of decimal number (ex. $5.99), output should be the nearest integer (0.5 rounds up to 1).
+    
+    Input
+    The function will receive an array of strings, each with the name of a fruit. 
+    The recognition of names should be case insensitive.There is no case of an empty array input.
+    """
+
+    regular = re.compile(r'(Banana)|(Orange)|(Apple)|(Lemon)|(Grapes)', re.I)  # $5 per each
+    special = re.compile(r'(Avocado)|(Strawberry)|(Mango)', re.I)  # $7 per each
+    per_5 = len(regular.findall(','.join(arr)))
+    per_7 = len(special.findall(','.join(arr)))
+    per_9 = len(arr) - per_5 - per_7
+    return round((5 * per_5 + 7 * per_7 + 9 * per_9) / len(arr))
+
