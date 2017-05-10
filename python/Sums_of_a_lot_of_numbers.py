c5011b0600b666e36c993587b5e7abf8bdf0c89b
Kou <bybynov09@gmail.com>
2017-05-11 01:14:10 +0800

    Change the pattern of judge.

--- python/Sums_of_a_lot_of_numbers.py
+++ python/Sums_of_a_lot_of_numbers.py
@@ -6,4 +6,4 @@ def f(n):
     Verify that 'n' is a valid positive integer number. 
     If not, return false (None for Python)
     """
-    return sum([x for x in xrange(n + 1) if isinstance(n, int) and n > 0]) or None
+    return sum(xrange(n + 1)) if (n > 0 and isinstance(n, int)) else None
