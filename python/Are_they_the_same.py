5640107b33d6f3eed7d099bcad031929124db994
Kou <bybynov09@gmail.com>
2018-04-21 14:28:54 +0800

    compare 2 arrays; whether b is a squared.

---
+++ python/Are_they_the_same.py
@@ -0,0 +1,46 @@
+"""
+Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If we change the first number to something else, comp may not return true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] (all languages except R, Shell). a or b might be nil or null or None (except in Haskell, Elixir, C++, Rust, R, Shell).
+
+If a or b are nil (or null or None), the problem doesn't make sense so return false.
+
+If a or b are empty the result is evident by itself.
+"""
+
+
+def comp(array1, array2):
+    # Time: 537ms
+    if array1 is None or array2 is None:
+        return False
+    elif len(array1) != len(array2):
+        return False
+    else:
+        arr1, arr2 = sorted(array1), sorted(array2)
+        for i in range(len(arr1)):
+            if arr1[i] * arr1[i] != arr2[i]:
+                return False
+        return True
+
+if __name__ == '__main__':
+    pass
