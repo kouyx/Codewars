004d09270ef697422c0da43c6d60e6a884ceacb5
Kou <bybynov09@gmail.com>
2018-04-22 13:32:50 +0800

    Deal with hash-table

---
+++ Leetcode/linked-list-cycle-ii.py
@@ -0,0 +1,30 @@
+"""
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+Follow up:
+Can you solve it without using extra space?
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        hash1 = set()
+        while head and head.next:
+            if head in hash1:
+                return head
+            else:
+                hash1.add(head)
+                head = head.next
+        return None
